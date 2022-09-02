770519ebff022e1da12c44cddef5fbe15fb2f305
svshyam91 <svshyam97@gmail.com>
2022-09-02 18:44:24 +0530

    Installed body-parser and parsed request body with bodyparser | Handled Post request and Get request with middleware

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/users", (req, res, next) => {
   res.send("<h1>This is Users Page.</h1>");
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add</button></form>"
+  );
 });
 
 app.use("/", (req, res, next) => {
