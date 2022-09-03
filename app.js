a216afff66e2847487dab5b44b44fd73cae045ab
svshyam91 <svshyam97@gmail.com>
2022-09-03 11:38:47 +0530

    Added Page Not Found

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(shopRoutes);
 app.use(adminRoutes);
 
+app.use((req, res, next) => {
+  res.send("<h1>Page not found</h1>");
+});
+
 app.listen(3001);
