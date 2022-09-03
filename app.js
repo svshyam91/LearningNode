c802693eb6741199e86c80814e68110473f50377
svshyam91 <svshyam97@gmail.com>
2022-09-03 11:33:40 +0530

    Used express.Router and split files based on routes

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/users", (req, res, next) => {
-  res.send("<h1>This is Users Page.</h1>");
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add</button></form>"
-  );
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>This is Home Page.</h1>");
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(3001);
