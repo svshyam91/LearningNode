0bf45cd08b79a07b168aef7645583f206e523a75
svshyam91 <svshyam97@gmail.com>
2022-09-13 21:03:07 +0530

    synced html and css files tutorial

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "views", "pageNotFound.html"));
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3001);
