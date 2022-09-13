9958defafe75a93096a27866106985a998b01c75
svshyam91 <svshyam97@gmail.com>
2022-09-13 18:16:29 +0530

    handled css file statically

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
