af28cc0b92b7eb95c894101a0bda42b377e354e2
svshyam91 <svshyam97@gmail.com>
2022-09-18 11:59:01 +0530

    Added Product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
     res.render("add-product.ejs", {
@@ -11,16 +11,16 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
     res.render("shop.ejs", {
-        prods: products,
+        prods: Product.fetchAll(),
         pageTitle: "Shop",
         path: "/",
-        hasProducts: products.length > 0,
         activeShop: true,
         shopCSS: true,
     });
